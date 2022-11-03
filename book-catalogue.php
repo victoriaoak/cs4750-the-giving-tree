abcbc91ee859bb83b453b806c6cee55d65f1c5b8
victoriaoak <89366891+victoriaoak@users.noreply.github.com>
2022-11-02 22:09:00 -0400

    functional add/update/delete book, start account database

--- book-catalogue.php
+++ book-catalogue.php
@@ -7,9 +7,29 @@ $book_to_update = null;
 ?>
 
 <?php
-
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  if (!empty($_POST['btnAction']) && $_POST['btnAction'] =='Add') 
+  {
+      addBook($_POST['title'], $_POST['author'], $_POST['genre'], $_POST['avg_rating'], $_POST['quantity'], $_POST['in_stock']);
+      $list_of_books = getAllBooks();  
+  }
+  else if (!empty($_POST['btnAction']) && $_POST['btnAction'] =='Update')
+  {
+      $book_to_update = getBookByTitleAuthor($_POST['book_title_to_update'], $_POST['book_author_to_update']);
+  }
+  else if (!empty($_POST['btnAction']) && $_POST['btnAction'] =='Delete') 
+  {
+      deleteBook($_POST['book_title_to_delete'], $_POST['book_author_to_delete']);
+      $list_of_books = getAllBooks();
+  }
+  if (!empty($_POST['btnAction']) && $_POST['btnAction'] =='Confirm update')
+  {
+    updateBook($_POST['title'], $_POST['author'], $_POST['genre'], $_POST['avg_rating'], $_POST['quantity'], $_POST['in_stock']);
+    $list_of_books = getAllBooks();
+  }
+}
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -37,6 +57,52 @@ $book_to_update = null;
 </div>
 <br/>
 
+<form name="mainForm" action="book-catalogue.php" method="post">   
+  <div class="row mb-3 mx-3">
+    Title:
+    <input type="text" class="form-control" name="title" required 
+          value="<?php if ($book_to_update!=null) echo $book_to_update['title'] ?>"
+    />            
+  </div>  
+  <div class="row mb-3 mx-3">
+    Author:
+    <input type="text" class="form-control" name="author" required 
+    value="<?php if ($book_to_update!=null) echo $book_to_update['author'] ?>"
+    />            
+  </div>
+  <div class="row mb-3 mx-3">
+    Genre:
+    <input type="text" class="form-control" name="genre" 
+    value="<?php if ($book_to_update!=null) echo $book_to_update['genre'] ?>"
+    />            
+  </div>  
+  <div class="row mb-3 mx-3">
+    Average Rating:
+    <input type="number" max="5" min="0" step="0.01" class="form-control" name="avg_rating" 
+    value="<?php if ($book_to_update!=null) echo $book_to_update['avg_rating'] ?>"
+    />            
+  </div>
+  <div class="row mb-3 mx-3">
+    Quantity:
+    <input type="number" min="0" class="form-control" name="quantity" 
+    value="<?php if ($book_to_update!=null) echo $book_to_update['quantity'] ?>"
+    />            
+  </div>
+  <div class="row mb-3 mx-3">
+    In Stock:
+    <input type="number" max="1" min="0" class="form-control" name="in_stock" 
+    value="<?php if ($book_to_update!=null) echo $book_to_update['in_stock'] ?>"
+    />            
+  </div>     
+  <div>
+    <input type="submit" value="Add" name="btnAction" class="btn btn-dark" 
+           title="Insert the book into the database" />            
+    <input type="submit" value="Confirm update" name="btnAction" class="btn btn-primary" 
+           title="Update book" />            
+  </div>  
+
+</form>   
+
 <div class="row justify-content-center">  
 <table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
   <thead>
@@ -61,19 +127,23 @@ $book_to_update = null;
      <td><?php echo $book_info['quantity']; ?></td>
      <td><?php echo $book_info['in_stock']; ?></td>                     
      <td>
-        <form action="book-info-form.php" method="post">
+        <form action="book-catalogue.php" method="post">
           <input type="submit" value="Update" name="btnAction" class="btn btn-primary" 
                 title="Click to update this book" />
-          <input type="hidden" name="book_to_update" 
-                value="<?php echo $book_info['title'], $book_info['author']; ?>" />                
+          <input type="hidden" name="book_title_to_update" 
+                value="<?php echo $book_info['title']; ?>" />
+          <input type="hidden" name="book_author_to_update" 
+                value="<?php echo $book_info['author']; ?>" />                 
         </form>
      </td>
      <td>
         <form action="book-catalogue.php" method="post">
           <input type="submit" value="Delete" name="btnAction" class="btn btn-danger" 
                 title="Click to remove this book from the database" />
-          <input type="hidden" name="book_to_delete" 
-                value="<?php echo $book_info['title'], $book_info['author']; ?>" />                
+          <input type="hidden" name="book_title_to_delete" 
+                value="<?php echo $book_info['title']; ?>" />
+          <input type="hidden" name="book_author_to_delete" 
+                value="<?php echo $book_info['author']; ?>" />                   
         </form>
      </td>
      <td>
