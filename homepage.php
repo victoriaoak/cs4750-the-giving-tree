0433de1cf738469cbd1cc67f84e97f24a32c0ac5
victoriaoak <89366891+victoriaoak@users.noreply.github.com>
2022-11-03 16:51:39 -0400

    book forms, customer, getUser

--- homepage.php
+++ homepage.php
@@ -1,5 +1,15 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <meta charset="UTF-8">  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="your name">
+  <meta name="description" content="include some description about your page">      
+  <title>The Giving Tree</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+</head>
 
 <body>
 <?php include('header.html') ?> 
